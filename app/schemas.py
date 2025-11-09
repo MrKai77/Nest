9e006c7a7400551f20751c14b0089222da2fabf0
bailey li <bailey.yao@ucalgary.ca>
2025-11-09 01:07:24 -0700

    changes to accomodate the vector weightings

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,6 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict
 from typing import Optional, List
+
 
 class DatabaseListing(BaseModel):
     id: str
@@ -20,11 +21,28 @@ class DatabaseListing(BaseModel):
     backyard: Optional[bool] = None
     garage: Optional[bool] = None
 
-    weighted_score: float
     updated_at: str
+    weighted_score: List[float] = Field(alias="weighted_score")
+    model_config = ConfigDict(validate_by_name=True)
+
+
+class CreateListingRequest(BaseModel):
+    price: float
+    date_listed: str
+    image_url: Optional[str] = None
+
+    longitude: float
+    latitude: float
+    address: str
+
+    square_footage: Optional[int] = None
+    bathroom_num: Optional[int] = Field(None, alias="bathroom_num")
+    bedrooms_num: Optional[int] = Field(None, alias="bedrooms_num")
+    backyard: Optional[bool] = None
+    garage: Optional[bool] = None
+    description: Optional[str] = None
 
 class Listing(BaseModel):
-    id: str
 
     price: float
     date_listed: str
@@ -43,6 +61,9 @@ class Listing(BaseModel):
     garage: Optional[bool] = None
 
 class SearchRequest(BaseModel):
+
+    description: Optional[str] = None
+    
     # Location fields
     address: str
     longitude: float
